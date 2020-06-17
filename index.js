34882849640d1740d661cf1892d4e9eba10b7378
asdfnmb24040 <asdfnmb24040@gmail.com>
2020-06-17 17:15:03 +0800

    add support for connStr2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/db')();//連接db
 require('./startup/routes')(app,express);//引入router
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);//
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`正在聆聽port ${port}`)); 
--- startup/db.js
+++ startup/db.js
@@ -6,6 +6,8 @@ module.exports = function () {
     //連接
     const db = config.get('db');
 
+    console.log('connect to =>>',db);
+    
     mongoose.connect(db)
         .then(() => winston.info(`connect to ${db}...`));
     // .catch給index.js抓
